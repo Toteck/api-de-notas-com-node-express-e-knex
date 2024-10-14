944de92091ac7a2e5053262c72815206d0a8a26c
mateus <mateusweslley14@gmail.com>
2024-10-14 11:13:12 -0300

    verificando se o usuário existe na base de dados

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,7 +1,20 @@
+// importação da conexão com o banco de dados
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
-    return response.json({ email, password });
+
+    // Procura o usuário no banco de dados
+    const user = await knex("users").where({ email }).first();
+
+    // Se usuário não existe lançe uma exceção
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorretos", 401);
+    }
+
+    return response.json(user);
   }
 }
 
